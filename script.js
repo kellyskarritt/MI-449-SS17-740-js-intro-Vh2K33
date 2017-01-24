afb59c629e459d4b77851abdb037c34400c8fd31
kellyskarritt <skarritt@msu.edu>
2017-01-24 15:21:20 -0500

    js audio buttons

---
+++ script.js
@@ -0,0 +1,17 @@
+var hihat = new Audio("hihat.wav") ;
+
+function hihatPlay() {
+  hihat.play();
+}
+
+var kick = new Audio("kick.wav") ;
+
+function kickPlay() {
+  kick.play();
+}
+
+var ride = new Audio("ride.wav") ;
+
+function ridePlay() {
+  ride.play();
+}
