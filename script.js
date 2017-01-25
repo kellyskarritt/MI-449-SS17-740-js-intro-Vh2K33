b47555c2dc21995b7508304e79ee2dbd4a8079ac
kellyskarritt <skarritt@msu.edu>
2017-01-24 19:08:17 -0500

    changes

--- script.js
+++ script.js
@@ -15,3 +15,11 @@ var ride = new Audio("ride.wav") ;
 function ridePlay() {
   ride.play();
 }
+
+document.getElementById("hihat").addEventListener("click", hihatPlay);
+document.getElementById("kick").addEventListener("click", kickPlay);
+document.getElementById("ride").addEventListener("click", ridePlay);
+
+document.getElementById("hihat").addEventListener("mouseenter", hihatPlay);
+document.getElementById("kick").addEventListener("mouseenter", kickPlay);
+document.getElementById("ride").addEventListener("mouseenter", ridePlay);
